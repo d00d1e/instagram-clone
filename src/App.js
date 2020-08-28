96dcb1f2cc3d089cbe40a568f09d89a3c4a132f7
d00d1e <lee.julianay@gmail.com>
2020-08-28 14:40:47 -0700

    Firebase authentication for signup/signin/logout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,161 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post';
+import { db, auth } from './firebase';
+
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, Input } from '@material-ui/core';
+
+// Modal control
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      username: "JLee",
-      caption: "First post: ReactJS guys!",
-      imageUrl: "https://www.freecodecamp.org/news/content/images/size/w1000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
-    },
-    {
-      username: "mooose",
-      caption: "Meow meow meow meow",
-      imageUrl: "https://images.unsplash.com/photo-1541781774459-bb2af2f05b55?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1942&q=80"
-    },
-    {
-      username: "lemon_lyfe",
-      caption: "When life gives you lemons...",
-      imageUrl: "https://images.unsplash.com/photo-1454944338482-a69bb95894af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80"
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+  const [posts, setPosts] = useState([]); 
+  const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('false');  
+  const [ user, setUser] = useState(null);
+
+  // runs after first render AND after every update
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // user has logged in (persistent login)
+        setUser(authUser);
+        console.log(authUser);
+      } else {
+        // user has logged out
+        setUser(null);
+      }
+    })
+    return () => {
+      unsubscribe();
     }
-  ]); 
+  }, [user, username]);
+
+
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot => {
+      // runs every time a new post is added 
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
+    })
+  }, [])
+
+  const signUp = (event) => {
+    event.preventDefault();
+
+    auth.createUserWithEmailAndPassword(email, password)
+    .then((authUser) => {
+      return authUser.user.updateProfile({
+        displayName: username
+      })
+    })
+    .catch((error) => alert(error.message))
+  }
+
+  const signIn = (event) => {
+    event.preventDefault();
+   
+    auth.signInWithEmailAndPassword(email, password)
+    .catch((error) => alert(error.message))
+
+    setOpenSignIn(false);
+  }
 
   return (
     <div className="App">
+
+      <Modal open={open} onClose={() => setOpen(false)} >
+        <div style={modalStyle} className={classes.paper}>
+          <center>
+            <img 
+              className="app__headerImage" 
+              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+              alt=""
+            />
+          </center>
+          <form className="app__signup">
+            <Input 
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input 
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input 
+              placeholder="[password]"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            
+            <Button onClick={signUp}>Sign Up</Button>
+          </form>
+        </div> 
+      </Modal>
+
+      <Modal open={openSignIn} onClose={() => setOpenSignIn(false)} >
+        <div style={modalStyle} className={classes.paper}>
+          <center>
+            <img 
+              className="app__headerImage" 
+              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+              alt=""
+            />
+          </center>
+          <form className="app__signin">
+            <Input 
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input 
+              placeholder="[password]"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          
+          <Button type="submit" onClick={signIn}>Sign In</Button>
+            
+          </form>
+        </div> 
+      </Modal>
+
       <div className="app__header">
         <img 
           className="app__headerImage" 
@@ -31,11 +164,19 @@ function App() {
         />
       </div>
 
+      {user ? (
+        <Button onClick={() => auth.signOut()}>Logout</Button>
+        ) : (
+          <div className="app__loginContainer">
+            <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+            <Button onClick={() => setOpen(false)}>Sign Up</Button>
+          </div>
+      )}
       <h1>Instagram Clone with React!</h1>
 
       {
-        posts.map(post => {
-          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+        posts.map(({id, post}) => {
+          return <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
         })
       }
 
